440d07b77a662ed4de5ccb53b872cb5fc8a6e656
PatricLB <Patriclovgrenb@gmail.com>
2022-10-06 23:11:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Shapes;
+
+Shape[] shapes = new Shape[21];
+
+
+for (int i = 0; i < 21; i++)
+{
+    shapes[i] = Shape.GenerateShape();
+}
+
+float totalArea = 0;
+
+for (int i = 0; i < shapes.Length - 1; i++)
+{
+    Console.WriteLine($"{i + 1}: {shapes[i]}");
+    totalArea += shapes[i].Area;
+}
+totalArea /= shapes.Length - 1;
+totalArea = (float)Math.Round(totalArea, 1);
+Console.WriteLine($"\nMedelvärdet på arean är: {totalArea} enheter");
---
+++ Shapes/Rectangles.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+
+namespace Shapes
+{
+    public class Rectangles : Shape2D
+    {
+
+        private Vector2 _size;
+        public bool isSquare = false;
+
+
+        public override float Circumference
+        {
+
+            get { return 2 * (_size.X + _size.Y); }
+
+        }
+
+        public Rectangles(Vector2 center, Vector2 sida)
+        {
+            Vector3 vector2till3 = new Vector3();
+            vector2till3.X = center.X;
+            vector2till3.Y = center.Y;
+            vector2till3.Z = 0.0f;
+            _center = vector2till3;
+
+            _size = sida;
+            _area = _size.X * _size.Y;
+
+            if (_size.X.Equals(_size.Y))
+            {
+                this.isSquare = true;
+            }
+
+        }
+        public Rectangles(Vector2 center, float width)
+        {
+            Vector3 vector2till3 = new Vector3();
+            vector2till3.X = center.X;
+            vector2till3.Y = center.Y;
+            vector2till3.Z = 0.0f;
+            _center = vector2till3;
+
+            _area = width * width;
+
+            _size.Y = width;
+            _size.X = width;
+            this.isSquare = true;
+        }
+
+        public override string ToString()
+        {
+            string returneraRektangel = $"rectangle @({_center.X}, {_center.Y}): w = {this._size.X} h = {this._size.Y}";
+
+            if (isSquare)
+            {
+                returneraRektangel = $"square @({_center.X}, {_center.Y}): w = {this._size.X} h = {this._size.Y}";
+            }
+
+            return returneraRektangel;
+        }
+
+    }
+}
